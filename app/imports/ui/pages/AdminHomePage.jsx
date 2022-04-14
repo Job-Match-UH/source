4782cdd2de7e853c51632a8f22064f46c9a71170
Michelle Leano <leanomc@hawaii.edu>
2022-04-14 08:48:43 -1000

    Updated Admin Home Page

--- app/imports/ui/pages/AdminHomePage.jsx
+++ app/imports/ui/pages/AdminHomePage.jsx
@@ -1,39 +1,87 @@
 import React from 'react';
-import { Header, Card, Container, Button, Image } from 'semantic-ui-react';
+import { Header, Card, Button, Image, Container } from 'semantic-ui-react';
 
 /** Renders a table containing all of the Stuff documents. Use <StuffItem> to render each row. */
 class AdminHomePage extends React.Component {
   render() {
     return (
-      <div className='admin-home'>
-        <Container id='admin-home'>
-          <Header inverted as="h2" textAlign="center">Admin Home</Header>
-          <Card.Group centered>
+      <Container textAlign='center'>
+        <Header as='h1'>Admin Home Page</Header>
+        <Card.Group centered>
+          <Card>
             <Card.Content>
               <Image
                 floated='right'
                 size='mini'
-                src='https://philipmjohnson.github.io/images/philip2.jpeg'
+                src='https://react.semantic-ui.com/images/avatar/large/steve.jpg'
               />
-              <Card.Header>Philip Johnson</Card.Header>
-              <Card.Meta>Friends of Brandon</Card.Meta>
+              <Card.Header>Pear Inc.</Card.Header>
+              <Card.Meta>Company</Card.Meta>
               <Card.Description>
-              Philip wants to add you to the group <strong>best friends</strong>
+            Overpriced tech retail store.
               </Card.Description>
             </Card.Content>
             <Card.Content extra>
               <div className='ui two buttons'>
-                <Button basic color='green'>
-                Approve
+                <Button basic color='grey'>
+              View
                 </Button>
                 <Button basic color='red'>
-                Decline
+              Delete
                 </Button>
               </div>
             </Card.Content>
-          </Card.Group>
-        </Container>
-      </div>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='mini'
+                src='https://react.semantic-ui.com/images/avatar/large/molly.png'
+              />
+              <Card.Header>Molly Thomas</Card.Header>
+              <Card.Meta>Student</Card.Meta>
+              <Card.Description>
+            Senior, Computer Science major, expected graduation Spring 2023.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className='ui two buttons'>
+                <Button basic color='grey'>
+              View
+                </Button>
+                <Button basic color='red'>
+              Delete
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+          <Card>
+            <Card.Content>
+              <Image
+                floated='right'
+                size='mini'
+                src='https://react.semantic-ui.com/images/avatar/large/jenny.jpg'
+              />
+              <Card.Header>Jenny Lawrence</Card.Header>
+              <Card.Meta>Student</Card.Meta>
+              <Card.Description>
+            Graduate student, BS in Sociology, BS in Data Science.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <div className='ui two buttons'>
+                <Button basic color='grey'>
+              View
+                </Button>
+                <Button basic color='red'>
+              Delete
+                </Button>
+              </div>
+            </Card.Content>
+          </Card>
+        </Card.Group>
+      </Container>
     );
   }
 }

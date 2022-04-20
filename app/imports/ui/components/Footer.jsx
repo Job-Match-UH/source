39a4bd901752457b856bf23eb83a72c907f5e420
C Arakaki <ca234@hawaii.edu>
2022-04-20 13:48:50 -1000

    Update Footer.jsx

--- app/imports/ui/components/Footer.jsx
+++ app/imports/ui/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 class Footer extends React.Component {
   render() {
+    const menuStyle = { topPadding: '15px', textAlign: 'center', color: 'white', backgroundColor: '#145714', fontFamily: 'Playfair Display' };
     return (
       <footer>
-        <div className="ui center aligned container">
-          <hr />
+        <div style={menuStyle}>
               Department of Information and Computer Sciences <br />
               University of Hawaii<br />
               Honolulu, HI 96822 <br />

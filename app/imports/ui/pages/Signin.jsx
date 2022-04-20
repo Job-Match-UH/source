66aa00ed348bc938de51c6d6b1047af84916d81d
C Arakaki <ca234@hawaii.edu>
2022-04-19 18:49:26 -1000

    Update Signin.jsx

--- app/imports/ui/pages/Signin.jsx
+++ app/imports/ui/pages/Signin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, Redirect } from 'react-router-dom';
 import { Meteor } from 'meteor/meteor';
-import { Button, Form, Grid, Header, Message, Segment, Divider } from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Message, Segment, Divider, Container } from 'semantic-ui-react';
 
 /**
  * Signin page overrides the form’s submit event and call Meteor’s loginWithPassword().
@@ -42,93 +42,95 @@ export default class Signin extends React.Component {
     }
     // Otherwise return the Login form.
     return (
-      <Segment placeholder id="signin-page">
-        <Grid columns={2} relaxed='very' stackable>
-          <Grid.Column>
-            <Header as="h2" textAlign="center" className='cp-text'>
+      <Container>
+        <Segment placeholder id="signin-page">
+          <Grid columns={2} relaxed='very' stackable>
+            <Grid.Column>
+              <Header as="h2" textAlign="center" className='cp-text'>
               Student Login
-            </Header>
-            <Form onSubmit={this.submit} className='cp-text'>
-              <Form.Input
-                icon='user'
-                iconPosition='left'
-                label='Email'
-                id="signin-form-email"
-                name="email"
-                type="email"
-                placeholder="E-mail address"
-                onChange={this.handleChange}
-              />
-              <Form.Input
-                icon='lock'
-                iconPosition='left'
-                label='Password'
-                type='password'
-                id="signin-form-password"
-                name="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-              <Button id="signin-form-submit" content='Login' primary />
-              <Message attached color='green'>
-                <Link to="/signup">Click here to Register as a Student</Link>
-              </Message>
-              {this.state.error === '' ? (
-                ''
-              ) : (
-                <Message
-                  error
-                  header="Login was not successful"
-                  content={this.state.error}
+              </Header>
+              <Form onSubmit={this.submit} className='cp-text'>
+                <Form.Input
+                  icon='user'
+                  iconPosition='left'
+                  label='Email'
+                  id="signin-form-email"
+                  name="email"
+                  type="email"
+                  placeholder="E-mail address"
+                  onChange={this.handleChange}
                 />
-              )}
-            </Form>
-          </Grid.Column>
+                <Form.Input
+                  icon='lock'
+                  iconPosition='left'
+                  label='Password'
+                  type='password'
+                  id="signin-form-password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+                <Button id="signin-form-submit" content='Login' primary />
+                <Message attached color='green'>
+                  <Link to="/signup">Click here to Register as a Student</Link>
+                </Message>
+                {this.state.error === '' ? (
+                  ''
+                ) : (
+                  <Message
+                    error
+                    header="Login was not successful"
+                    content={this.state.error}
+                  />
+                )}
+              </Form>
+            </Grid.Column>
 
-          <Grid.Column verticalAlign='middle'>
-            <Header as="h2" textAlign="center" className='cp-text'>
+            <Grid.Column verticalAlign='middle'>
+              <Header as="h2" textAlign="center" className='cp-text'>
                 Company Login
-            </Header>
-            <Form onSubmit={this.submit} className='cp-text'>
-              <Form.Input
-                icon='user'
-                iconPosition='left'
-                label='Email'
-                id="signin-form-email"
-                name="email"
-                type="email"
-                placeholder="E-mail address"
-                onChange={this.handleChange}
-              />
-              <Form.Input
-                icon='lock'
-                iconPosition='left'
-                label='Password'
-                type='password'
-                id="signin-form-password"
-                name="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-              />
-              <Button id="signin-form-submit" content='Login' primary />
-              <Message attached color='green'>
-                <Link to="/signup">Click here to Register as a Company</Link>
-              </Message>
-              {this.state.error === '' ? (
-                ''
-              ) : (
-                <Message
-                  error
-                  header="Login was not successful"
-                  content={this.state.error}
+              </Header>
+              <Form onSubmit={this.submit} className='cp-text'>
+                <Form.Input
+                  icon='user'
+                  iconPosition='left'
+                  label='Email'
+                  id="signin-form-email"
+                  name="email"
+                  type="email"
+                  placeholder="E-mail address"
+                  onChange={this.handleChange}
                 />
-              )}
-            </Form>
-          </Grid.Column>
-        </Grid>
+                <Form.Input
+                  icon='lock'
+                  iconPosition='left'
+                  label='Password'
+                  type='password'
+                  id="signin-form-password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                />
+                <Button id="signin-form-submit" content='Login' primary />
+                <Message attached color='green'>
+                  <Link to="/signup">Click here to Register as a Company</Link>
+                </Message>
+                {this.state.error === '' ? (
+                  ''
+                ) : (
+                  <Message
+                    error
+                    header="Login was not successful"
+                    content={this.state.error}
+                  />
+                )}
+              </Form>
+            </Grid.Column>
+          </Grid>
 
-        <Divider vertical>Or</Divider>
-      </Segment>
+          <Divider vertical>Or</Divider>
+        </Segment>
+      </Container>
     );
   }
 }

e1931114e7f9ca531481204b03c02c3de5cb6026
Edalaine Cadiena <ecadiena323@gmail.com>
2022-04-21 10:47:15 -1000

    #30 edit publications

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,28 +1,11 @@
 import { Meteor } from 'meteor/meteor';
-import { Stuffs } from '../../api/stuff/Stuff.js';
 import { Students } from '../../api/student/Student';
-
-// Initialize the database with a default data document.
-function addData(data) {
-  // eslint-disable-next-line no-console
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
-}
 
 // Initialize the database with a default data document.
 function addStudent(data) {
   // eslint-disable-next-line no-console
   console.log(`  Adding: ${data.name} (${data.owner})`);
   Students.collection.insert(data);
-}
-
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
-  if (Meteor.settings.defaultData) {
-    // eslint-disable-next-line no-console
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
-  }
 }
 
 // Initialize the StudentCollection if empty.

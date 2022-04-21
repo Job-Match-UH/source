716a70f9d4d12acee8f41fa4cd8a6498ef621f88
Gaila Galano <gailag@hawaii.edu>
2022-04-21 12:15:46 -1000

    Created Class Collection

--- app/imports/startup/server/Mongo.js
+++ app/imports/startup/server/Mongo.js
@@ -1,20 +1,17 @@
-/*
 import { Meteor } from 'meteor/meteor';
-// import { Stuffs } from '../../api/stuff/Stuff.js';
+import { Class } from '../../api/class/Class';
 
-/!* eslint-disable no-console *!/
+/* eslint-disable no-console */
 
 // Initialize the database with a default data document.
-function addData(data) {
-  console.log(`  Adding: ${data.name} (${data.owner})`);
-  Stuffs.collection.insert(data);
+function addClass(data) {
+  console.log(`  Adding: ${data.className} (${data.courseNumber})`);
+  Class.collection.insert(data);
 }
 
-// Initialize the StuffsCollection if empty.
-if (Stuffs.collection.find().count() === 0) {
+if (Class.collection.find().count() === 0) {
   if (Meteor.settings.defaultData) {
-    console.log('Creating default data.');
-    Meteor.settings.defaultData.map(data => addData(data));
+    console.log('Creating default class data.');
+    Meteor.settings.defaultData.map(data => addClass(data));
   }
 }
-*/
--- app/imports/ui/pages/ClassScheduleForm.jsx
+++ app/imports/ui/pages/ClassScheduleForm.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Container, Header, Form, Button, Input } from 'semantic-ui-react';
+import swal from 'sweetalert';
+import { Meteor } from 'meteor/meteor';
+import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
+import SimpleSchema from 'simpl-schema';
+import { Class } from '../../api/class/Class';
+
+const formSchema = new SimpleSchema({
+  className: String,
+  courseNumber: Number,
+  startMonth: String,
+  endMonth: String,
+  startYear: Number,
+  endYear: Number,
+});
+
+const bridge = new SimpleSchema2Bridge(formSchema);
 
 class ClassScheduleForm extends React.Component {
+  submit(data, formRef) {
+    const { className, courseNumber, startMonth, endMonth, startYear, endYear } = data;
+    const owner = Meteor.user().username;
+    Class.collection.insert({ className, courseNumber, startMonth, endMonth, startYear, endYear, owner },
+      (error) => {
+        if (error) {
+          swal('Error', error.message, 'error');
+        } else {
+          swal('Success', 'Item added successfully', 'success');
+          formRef.reset();
+        }
+      });
+  }
+
   render() {
+    let fRef = null;
     return (
       <Container>
         <Header as='h1' textAlign='center'>Class Schedule</Header>
-        <Form>
+        <Form ref={ref => { fRef = ref; }} schema={bridge} onSubmit={data => this.submit(data, fRef)}>
           <Form.Field>
             <label>Course Title</label>
             <input placeholder='Input class title here i.e. World History' />

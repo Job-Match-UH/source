5636458b40c75285a22b3364615aca824ef8df22
C Arakaki <ca234@hawaii.edu>
2022-05-09 22:00:02 -1000

    Update tests.testcafe.js

--- app/tests/tests.testcafe.js
+++ app/tests/tests.testcafe.js
@@ -130,7 +130,7 @@ test('Test viewstudentmatches page displays', async (testController) => {
   await viewStudentMatchesPage.isDisplayed(testController);
 });
 
-test.only('Test viewstudentprofile page displays', async (testController) => {
+test('Test viewstudentprofile page displays', async (testController) => {
   await navBar.gotoSigninPage(testController);
   await signinPage.signin(testController, credentials2.username, credentials2.password);
   await navBar.isLoggedIn(testController, credentials2.username);
